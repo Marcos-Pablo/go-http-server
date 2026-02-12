e1b144f44279e4f46fd6878247adb9fa47a1d86b
Marcos Pablo <marquin.pablo@gmail.com>
2026-02-12 14:39:48 -0300

    refactor: move api endpoints to /api/ path

--- main.go
+++ main.go
@@ -22,9 +22,9 @@ func main() {
 
 	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	server := http.Server{
 		Handler: mux,
